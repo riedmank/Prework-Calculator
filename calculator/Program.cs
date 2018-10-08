c2ee3305d96a5be46b57c390b3fefb71236a04c3
Kristofer Riedman <riedmank@hotmail.com>
2018-10-08 09:14:58 -0700

    methods setup

---
+++ calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static int Add(int x, int y) => x + y;
+
+        static int Subtract(int x, int y) => x - y;
+
+        static int Multiply(int x, int y) => x * y;
+
+        static double Divide(int x, int y) => (double)x / y;
+    }
+}
